a2b05a6a7a7e2afa5651208810ac36a0bed014b3
m4reQ <m4reQ@users.noreply.github.com>
2021-05-16 12:38:52 +0200

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -1,4 +1,5 @@
 source = """
+const CNST, word 64
 mov al, byte 5
 .Label
 	mov eax, al
@@ -8,11 +9,14 @@ mov al, byte 5
 	db "test", 0
 jmp $
 jeq .Label
-int
 .Func
-	mov eax, .String
+	mov eax, [.Number]
+	and CNST			; this is a comment
+	nop
 	ret
-cal .Func
+call .Func
+.Number
+	db 0x01
 hlt
 """
 # output: b"\xc2\x80@\x05\xc2\x80D@\xc2\x81D\xc2\x85B\x03test\x00B\x10E\x03\x04\xc2\x80D\x0b\x08G\x15\x03"
@@ -35,7 +39,7 @@ hlt
 #
 # preprocessor keywords:
 # db [*byte, optional:termination_character]: define bytes
-# const [type, value]: define constant
+# const [name, type value]: define constant
 #
 # types:
 # byte - 8 bits
@@ -70,6 +74,9 @@ hlt
 # 0 => is register index (1 if true)
 # 1 => argument type (0 for byte, 1 for word)
 # 2 - n => value
+#
+# comment structure:
+# ;[comment]
 #
 # register size: word
 #
@@ -122,21 +129,121 @@ hlt
 # nop - no operation, args: None
 # pop - pop data from stack, args: None
 # push - push data onto stack, args: [value]
-# pushf - push flags register onto stack, args: None
-# test - logical AND and set zero flag, args: [value or register, value or register]
+# test - logical AND and set zero flag if not equal, args: [value or register, value or register]
 
-class Operation(object):
-	"""
-	    10      000001 (1 byte)
-	    ^^      ^^^^^^
-	args count  opcode
-	"""
-	def __init__(self, mnemonic, opcode):
-		self.mnemonic = mnemonic
-		self.opcode = opcode
+OPERATIONS = {
+	"add":  0b10000000,
+	"adc":  0b10000001,
+	"div":  0b10000010,
+	"mul":  0b10000011,
+	"sub":  0b10000100,
+	"sbb":  0b10000101,
+	"and":  0b01000110,
+	"or":   0b01000111,
+	"xor":  0b01001000,
+	"sl":   0b01001001,
+	"sr":   0b01001010,
+	"not":  0b00001011,
+	"call": 0b01001100,
+	"ret":  0b00001101,
+	"sys":  0b01001110,
+	"inc":  0b01001111,
+	"dec":  0b01010000,
+	"hlt":  0b00010001,
+	"int":  0b01010010,
+	"jmp":  0b01010011,
+	"jeq":  0b01010100,
+	"jne":  0b01010101,
+	"mov":  0b10010110,
+	"nop":  0b00010111,
+	"pop":  0b00011000,
+	"push": 0b01011001,
+	"test": 0b10011010
+}
+
+DIRECTIVES = [
+	"db",
+	"const"
+]
+
+ARG_TYPE_BYTE = 0b00000000
+ARG_TYPE_WORD = 0b10000000
+
+class Assembly(object):
+	def __init__(self):
+		self.labels = {}
+		self.constants = {}
+		self.line_addresses = [0,]
+		self.binary = ""
+		self.size = 0
+
+class CompilerError(Exception):
+	def __init__(self, msg):
+		super().__init__(msg)
+
+def create_lines(text):
+	lines = [x for x in text.split('\n') if x != '']
+	newLines = []
+
+	for l in lines:
+		formatted = l.replace('\t', '')
+		if formatted.find(';') != -1:
+			formatted = formatted.split(';')[0]
+		newLines.append(formatted)
+
+	return newLines
+
+def handle_preprocessor_directive(line, assembly):
+	if line.startswith("db"):
+		line = line.replace("db", '')
+		line = line.replace(' ', '')
+		
+		args = line.split(',')
+		if len(args) not in [1, 2]:
+			raise CompilerError(f"Preprocessor error: Invalid 'db' args count: {len(args)}")
+		
+		new_line = args[0].replace('"', '')
+		if len(args) == 2:
+			new_line += chr(int(args[1]))
+
+		assembly.line_addresses.append(len(new_line))
+		return new_line
+	elif line.startswith("const"):
+		line = line.replace("const", '')
+		line = line.replace(' ', '')
+		
+		args = line.split(',')
+		if len(args) != 2:
+			raise CompilerError(f"Preprocessor error: Invalid 'const' args count: {len(args)}")
+
+		if args[1].startswith("byte"):
+			arg = args[1].replace("byte", '')
+			arg = int(arg) + ARG_TYPE_BYTE
+		elif args[1].startswith("word"):
+			arg = args[1].replace("word", '')
+			arg = int(arg) + ARG_TYPE_WORD
+		else:
+			raise CompilerError(f"Preprocessor error: invalid argument: '{args[1]}'")
+
+		const_name = args[0]
+		const_value = arg
+
+		assembly.constants[const_name] = const_value
+
+	return None
 	
-	def __repr__(self):
-		return f"{self.mnemonic}"
+
+def assemble(source):
+	assembly = Assembly()
+	lines = create_lines(source)
+	
+	for idx, line in enumerate(lines):
+		l = handle_preprocessor_directive(line, assembly)
+		if l:
+			lines[idx] = l
+	
+	return assembly
+
 
 class DBDirective(object):
 	def __init__(self, text, term_char):
@@ -146,7 +253,7 @@ class DBDirective(object):
 	def __repr__(self):
 		return f"[db] ({self.text}, {self.term_char})"
 
-class Assembly(object):
+class Assembly_OLD(object):
 	def __init__(self):
 		self.labels = {}
 		self.instructions = []
@@ -162,17 +269,7 @@ class Instruction(object):
 	def __repr__(self):
 		return f"[{self.op}] ({', '.join([str(x) for x in self.args])})"
 
-OPERATIONS = {
-	"mov": Operation("mov", 0b10000000),
-	"add": Operation("add", 0b10000001),
-	"jmp": Operation("jmp", 0b01000010),
-	"hlt": Operation("hlt", 0b00000011),
-	"int": Operation("int", 0b00000100),
-	"jeq": Operation("jeq", 0b01000101),
-	"jne": Operation("jne", 0b01000110),
-	"cal": Operation("cal", 0b01000111),
-	"ret": Operation("ret", 0b00001000)
-}
+
 
 REG_INDICES = {
 	"ax": 0,
@@ -208,7 +305,7 @@ def write_to_file(text, filename):
 	with open(filename, "wb") as f:
 		f.write(text.encode("UTF-8"))
 
-def assemble(text):
+def assemble_old(text):
 	assembly = Assembly()
 
 	lines = split(text)
